cce3a225b5f310706230bd70512bb1d1fce449ac
thiagosantosdev <thiagosantosjss@gmail.com>
2026-05-16 16:53:24 -0300

    Conhecendo e implementando o padrão Repository

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class CozinhaRepositoryImpl implements CozinhaRepository{
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@Override
+	public List<Cozinha> listar(){
+		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+	}
+	@Override
+	public Cozinha buscar(Long id) {
+		return manager.find(Cozinha.class, id);
+	}
+    @Transactional
+    @Override
+    public Cozinha salvar(Cozinha cozinha) {
+    	return manager.merge(cozinha);
+    }
+    
+	@Transactional
+	@Override
+	public void remover(Cozinha cozinha) {
+		cozinha = buscar(cozinha.getId());
+		manager.remove(cozinha);
+	}
+
+	
+
+	
+
+}
---
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,5 @@
+package com.algaworks.algafood.jpa;
+
+public class CadastroCozinha {
+
+}
