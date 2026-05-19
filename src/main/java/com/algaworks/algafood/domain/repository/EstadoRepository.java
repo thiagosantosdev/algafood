a022f3d1b297c8adf8a7280ac08be5354570a545
thiagosantosdev <thiagosantosjss@gmail.com>
2026-05-19 09:07:53 -0300

    Desafio: collection resource de estados

--- src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
@@ -1,10 +1,15 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+
 import com.algaworks.algafood.domain.model.Estado;
 
 public interface EstadoRepository {
 
 	Estado salvar(Estado estado);
 
+	List<Estado> listar();
+
 	
 }
+
--- src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.infrastructure.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Estado;
@@ -20,4 +22,10 @@ public class EstadoRepositoryImpl implements EstadoRepository {
 	    public Estado salvar(Estado estado) {
 	    	return manager.merge(estado);
 	    }
+
+		@Override
+		public List<Estado> listar() {
+
+			return manager.createQuery("from Estado", Estado.class).getResultList();
+		}
 }
