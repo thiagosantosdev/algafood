5e6191589391b23cd16d92be57118e0f6fe65136
thiagosantosdev <thiagosantosjss@gmail.com>
2026-05-19 08:26:50 -0300

    Modelando e requisitando um Collection Resource com GET

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,6 @@
 insert into cozinha (id, nome) values (1,'Portuguesa');
 insert into cozinha (id, nome) values (2, 'Italiana');
+insert into cozinha (id, nome) values (3, 'Brasileira');
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Restaurante da João', 10, 1);
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Restaurante da Maria', 10, 2);
 
