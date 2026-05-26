fdac491f7f50fb4d9e2e65ecb449a579b0ea0600
thiagosantosdev <thiagosantosjss@gmail.com>
2026-05-26 16:20:03 -0300

    Desafio: Modelando e implementando a atualização de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -35,6 +35,19 @@ public class CadastroRestauranteService {
 		return restauranteRepository.salvar(restaurante);
 	}
 	
+	public Restaurante atualizar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if(cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com código %d ", cozinhaId));
+		}
+		restaurante.setCozinha(cozinha);
+		
+		return restauranteRepository.salvar(restaurante);
+	}
+	
 	
 	
 	
